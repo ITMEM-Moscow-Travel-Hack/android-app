951d8dea7bc639433723dff3f3083996c49a87f5
gleb <gleb.demin.04@mail.ru>
2022-10-15 18:05:21 +0300

    rgegregrre

--- app/src/main/java/com/moscow/travel/hack/presentation/main/AppState.kt
+++ app/src/main/java/com/moscow/travel/hack/presentation/main/AppState.kt
@@ -24,6 +24,7 @@ class AppState(
 
     @Composable
     fun getStartDestination(): String {
+//        return GreetingSections.WELCOME.route
         return RecomendationsSections.TINDER.route
     }
 
--- app/src/main/java/com/moscow/travel/hack/presentation/view/PlaceCard.kt
+++ app/src/main/java/com/moscow/travel/hack/presentation/view/PlaceCard.kt
@@ -1,15 +1,15 @@
 package com.moscow.travel.hack.presentation.view
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -31,20 +31,22 @@ fun PlaceCard(
     Card(modifier.clickable(enabled = onClick != null) {
         onClick?.invoke(place.id)
     }) {
-        Box {
+        Column {
             AsyncImage(
                 model = ImageRequest.Builder(LocalContext.current)
                     .data(place.photoUri)
                     .crossfade(true)
                     .build(),
-                placeholder = painterResource(R.drawable.ic_launcher_background),
-                error = painterResource(R.drawable.ic_launcher_background),
+                placeholder = painterResource(R.drawable.ic_placeholder),
+                error = painterResource(R.drawable.ic_placeholder),
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .heightIn(180.dp, 200.dp)
             )
-            Scrim(Modifier.align(Alignment.BottomCenter))
-            Column(Modifier.align(Alignment.BottomStart)) {
+            Column(Modifier) {
                 Text(
                     text = "${place.name}, ${place.price.toInt()} руб.",
                     color = MaterialTheme.colors.onPrimary,
