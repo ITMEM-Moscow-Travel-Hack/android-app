487759ab29f2fc52559c73413dfdcb5df6b393bb
gleb <gleb.demin.04@mail.ru>
2022-10-16 05:37:44 +0300

    hotel screen

--- app/src/main/java/com/moscow/travel/hack/presentation/main/MTHApp.kt
+++ app/src/main/java/com/moscow/travel/hack/presentation/main/MTHApp.kt
@@ -1,7 +1,9 @@
 package com.moscow.travel.hack.presentation.main
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.moscow.travel.hack.presentation.theme.MoscowTravelHackAppTheme
 
 @Composable
@@ -9,8 +11,9 @@ fun MTHApp(appState: AppState) {
     MoscowTravelHackAppTheme {
         Scaffold(
             scaffoldState = appState.scaffoldState,
-        ) {
+        ) { innerPadding ->
             MTHNavHost(
+                modifier = Modifier.padding(innerPadding),
                 navController = appState.navController,
                 startDestination = appState.getStartDestination()
             )
