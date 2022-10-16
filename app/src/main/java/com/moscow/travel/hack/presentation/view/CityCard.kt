282f852fc74d36a1b747d8af20ae5abc27a20c78
Ilya Ponomarenko <is.ponomarenko222q@gmail.com>
2022-10-16 03:20:55 +0300

    city

--- app/src/main/java/com/moscow/travel/hack/presentation/view/CityCard.kt
+++ app/src/main/java/com/moscow/travel/hack/presentation/view/CityCard.kt
@@ -1,16 +1,15 @@
 package com.moscow.travel.hack.presentation.view
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -21,6 +20,7 @@ import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.moscow.travel.hack.R
 import com.moscow.travel.hack.domain.entity.City
+import com.moscow.travel.hack.presentation.theme.Background2
 
 @Composable
 fun CityCard(
@@ -28,8 +28,19 @@ fun CityCard(
     modifier: Modifier = Modifier,
     onCityClick: (Int) -> Unit,
 ) {
-    Card(modifier = Modifier.clickable { onCityClick(city.id) }) {
-        Row(modifier.fillMaxWidth()) {
+    Card(
+        modifier = Modifier
+            .clip(MaterialTheme.shapes.large)
+            .border(
+                2.5.dp,
+                color = MaterialTheme.colors.Background2,
+                shape = MaterialTheme.shapes.large
+            )
+            .clickable { onCityClick(city.id) }) {
+        Row(
+            modifier
+                .fillMaxWidth()
+                .padding(8.dp), verticalAlignment = Alignment.CenterVertically) {
             AsyncImage(
                 model = ImageRequest.Builder(LocalContext.current)
                     .data(city.photoUri)
@@ -40,26 +51,24 @@ fun CityCard(
                 contentDescription = null,
                 contentScale = ContentScale.Fit,
                 modifier = Modifier
-                    .size(85.dp)
+                    .size(90.dp)
+                    .clip(MaterialTheme.shapes.large)
             )
+            Spacer(modifier = Modifier.width(8.dp))
             Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(start = 16.dp)
+                modifier = Modifier.height(90.dp)
             ) {
                 Text(
                     text = city.name,
                     color = MaterialTheme.colors.onPrimary,
                     fontSize = 22.sp,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(10.dp)
+                    fontWeight = FontWeight.Bold
                 )
+                Spacer(modifier = Modifier.size(8.dp))
                 Text(
                     text = city.places.joinToString(", "),
                     color = MaterialTheme.colors.onPrimary,
-                    fontSize = 22.sp,
-                    fontWeight = FontWeight.Medium,
-                    modifier = Modifier.padding(10.dp)
+                    fontSize = 16.sp
                 )
             }
         }
