64a4473c14e1974b901849952b63fbbd333b350e
Ilya Ponomarenko <is.ponomarenko222q@gmail.com>
2022-10-16 04:13:40 +0300

    hotels

--- app/src/main/java/com/moscow/travel/hack/presentation/ui/greeting/recomendation/HotelsScreen.kt
+++ app/src/main/java/com/moscow/travel/hack/presentation/ui/greeting/recomendation/HotelsScreen.kt
@@ -1,21 +1,25 @@
 package com.moscow.travel.hack.presentation.ui.greeting.recomendation
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.moscow.travel.hack.domain.entity.hotels
 import com.moscow.travel.hack.presentation.view.HotelCard
+import com.moscow.travel.hack.presentation.view.SexyTextField
 
 @Composable
 fun HotelsScreen() {
-    LazyColumn {
-        items(hotels) {
-            HotelCard(hotel = it, onHotelClick = {})
-            Spacer(modifier = Modifier.height(8.dp))
+    Column(Modifier.padding(16.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        SexyTextField(modifier = Modifier.fillMaxWidth(), icon = Icons.Rounded.Search)
+        LazyColumn(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+            items(hotels) {
+                HotelCard(hotel = it, onHotelClick = {})
+            }
         }
     }
 }
